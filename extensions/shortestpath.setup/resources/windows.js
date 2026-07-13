d74c9cd0fb0c048a4b239c600306590cbed35eed
Kevin <370865552@qq.com>
2026-07-13 21:04:37 +0800

    fix(setup): remove unavailable release mirror

--- extensions/shortestpath.setup/resources/windows.js
+++ extensions/shortestpath.setup/resources/windows.js
@@ -6,7 +6,6 @@ exports.getPortableAssets = () => [
 	{
 		id: 'gcc',
 		urls: [
-			'https://mirrors.tuna.tsinghua.edu.cn/github-release/brechtsanders/winlibs_mingw/16.1.0posix-14.0.0-msvcrt-r3/winlibs-x86_64-posix-seh-gcc-16.1.0-mingw-w64msvcrt-14.0.0-r3.zip',
 			'https://github.com/brechtsanders/winlibs_mingw/releases/download/16.1.0posix-14.0.0-msvcrt-r3/winlibs-x86_64-posix-seh-gcc-16.1.0-mingw-w64msvcrt-14.0.0-r3.zip'
 		],
 		archiveName: 'winlibs-gcc.zip',
@@ -16,7 +15,6 @@ exports.getPortableAssets = () => [
 	{
 		id: 'clangd',
 		urls: [
-			'https://mirrors.tuna.tsinghua.edu.cn/github-release/clangd/clangd/22.1.6/clangd-windows-22.1.6.zip',
 			'https://github.com/clangd/clangd/releases/download/22.1.6/clangd-windows-22.1.6.zip'
 		],
 		archiveName: 'clangd-22.1.6.zip',
